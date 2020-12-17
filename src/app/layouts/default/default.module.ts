1771d9bc364817f256f3d35b9f024b5af2105a02
bilusau <45253385+bilusau@users.noreply.github.com>
2020-12-17 14:59:25 +0530

    Update default.module.ts

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
-import { DashboardComponent } from '../modules/dashboard/dashboard.component';
+import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
-import { PostsComponent } from '../modules/posts/posts.component';
-import { SharedModule } from '../shared/shared.module';
+import { PostsComponent } from '../../modules/posts/posts.component';
+import { SharedModule } from '../../shared/shared.module';
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
-import { DashboardService } from '../modules/dashboard.service';
+import { DashboardService } from '../../modules/dashboard.service';
 
 
 @NgModule({
